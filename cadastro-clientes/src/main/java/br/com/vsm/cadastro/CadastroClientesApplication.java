96307863c42c0528ea1499595f43936f6ef901d6
Roger Willian <roger.nizoli@gmail.com>
2016-04-06 23:05:18 -0300

    Teste Spring Security

--- cadastro-clientes/src/main/java/br/com/vsm/cadastro/CadastroClientesApplication.java
+++ cadastro-clientes/src/main/java/br/com/vsm/cadastro/CadastroClientesApplication.java
@@ -5,10 +5,7 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
@@ -23,11 +20,11 @@ public class CadastroClientesApplication {
 		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 
-	@Configuration
-	public static class MvcConfig extends WebMvcConfigurerAdapter {
-		@Override
-		public void addViewControllers(ViewControllerRegistry registry) {
-			registry.addRedirectViewController("/", "/clientes");
-		}
-	}
+	// @Configuration
+	// public static class MvcConfig extends WebMvcConfigurerAdapter {
+	// @Override
+	// public void addViewControllers(ViewControllerRegistry registry) {
+	// registry.addRedirectViewController("/", "/clientes");
+	// }
+	// }
 }
